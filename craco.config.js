3975029b6570b1befed0e9955e9886ccd5c73f20
David Ballester Mena <david.ballester.mena@gmail.com>
2021-10-04 07:16:51 +0200

    build: single bundle JS file

--- craco.config.js
+++ craco.config.js
@@ -4,4 +4,30 @@ module.exports = {
       plugins: [require("tailwindcss"), require("autoprefixer")],
     },
   },
+  webpack: {
+    configure: {
+      output: {
+        filename: "static/js/dots-map.js",
+      },
+      optimization: {
+        runtimeChunk: false,
+        splitChunks: {
+          chunks(chunk) {
+            return false;
+          },
+        },
+      },
+    },
+  },
+  plugins: [
+    {
+      plugin: {
+        overrideWebpackConfig: ({ webpackConfig }) => {
+          webpackConfig.plugins[5].options.filename = "static/css/dots-map.css";
+          return webpackConfig;
+        },
+      },
+      options: {},
+    },
+  ],
 };
