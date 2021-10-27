207e45cd420a955995e1cf0cc0ea7796beed66d5
lyricalpolymath <b25zero1@gmail.com>
2021-10-27 13:39:32 +0200

    chore: add subsaharan countries

--- src/data/regions.js
+++ src/data/regions.js
@@ -26,7 +26,7 @@ export default function getRegions() {
       {
         id: "sub-saharan_africa",
         readableName: "Sub-Saharan Africa",
-        countries: [],
+        countries: ["angola", "benin", "botswana", "burkina_faso","burundi","congo", "cameroon", "cape_verde", "comoros","central_sub-saharan_african_republic","chad", "democratic_republic_of_congo", "djibouti","equatorial_guinea","eritrea", "ethiopia", "eswatini", "gambia","gabon", "ghana", "guinea_bissau", "republic_of_guinea", "ivory_coast","kenya","lesotho","liberia","madagascar","mali", "malawi","mauritius","mauritania","mozambique", "namibia", "niger", "nigeria","rwanda", "sao_tome_and_principe", "senegal", "sierra_leone", "somalia","south_sudan","south_sub-saharan_africa", "sudan","tanzania", "togo","uganda", "zambia", "zimbabwe" ],
         relativePosition: {
           x: 0.54,
           y: 0.65,
